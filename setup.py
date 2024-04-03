f2c1264c63ecc42a2523a0c1591fcf4c284dc144
metalogico <michele.brandolin@gmail.com>
2024-04-03 15:34:02 +0200

    push version 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django_sonar',
-    version='0.1.1',
+    version='0.1.2',
     author='Metalogico',
     author_email='michele.brandolin@gmail.com',  # Replace with your email
     description='The missing debug tool for Django',
