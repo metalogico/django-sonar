f878348cbc8020a317d475bcf9721e5ee44f5c95
metalogico <michele.brandolin@gmail.com>
2024-04-04 12:49:50 +0200

    bump version 0.1.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django_sonar',
-    version='0.1.6',
+    version='0.1.7',
     author='Metalogico',
     author_email='michele.brandolin@gmail.com',
     description='The missing debug tool for Django',
